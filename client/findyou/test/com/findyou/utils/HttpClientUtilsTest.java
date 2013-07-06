3252b61fbf56eec42ba528b56960e26ff727fd52
hongliuliao <357857613@qq.com>
2013-07-06 14:40:46 +0800

    添加测试代码

--- client/findyou/test/com/findyou/utils/HttpClientUtilsTest.java
+++ client/findyou/test/com/findyou/utils/HttpClientUtilsTest.java
@@ -21,6 +21,8 @@ public class HttpClientUtilsTest extends AndroidTestCase {
 	public void testGetHttpGetResult() {
 		String requestUrl = "https://raw.github.com/hongliuliao/findyou/master/client/findyou/test/saveMyInfo.json";
 		Map<String, String> params = new HashMap<String, String>();
+		params.put("id", "111");
+		params.put("userId", "222");
 		String result = HttpClientUtils.getHttpGetResult(requestUrl, params);
 		Log.i("test", result);
 		assertTrue(result != null);
