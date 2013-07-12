ed568019d957f088d4a6d7099eb762f678912cd3
hongliuliao <357857613@qq.com>
2013-07-12 22:45:12 +0800

    去掉作者的信息

--- client/findyou/src/com/findyou/utils/DatabaseConfigUtil.java
+++ client/findyou/src/com/findyou/utils/DatabaseConfigUtil.java
@@ -5,13 +5,6 @@ import android.content.Context;
 import com.findyou.data.DataHelper;
 import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 
-/**
- * 生成ORM映射文件
- * 
- * @author 福建师范大学软件学院 陈贝、刘大刚
- * @since 2012-6-8
- * 
- */
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
 
 	public static DataHelper DATAHELPER;
